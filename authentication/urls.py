1b7ba0acbba35bd90590365f7c56678597dfcce4
Ahmad_Salman2021 <ahmad.salman@kuwaitnet.dev>
2022-12-14 11:36:14 +0200

    Feat

--- authentication/urls.py
+++ authentication/urls.py
@@ -20,7 +20,7 @@ router.register(r'wallet-logs', WalletLogView, basename='wallet-logs')
 urlpatterns = [
     path('password-change/', ChangePasswordView.as_view(), name='password-change'),
     # path('user-creation/', UserCreateView.as_view(), name='user-creation'),
-    path('login/', TokenCreateView.as_view(), name='user-login'),
+    path('login', TokenCreateView.as_view(), name='user-login'),
     path('', include(router.urls)),
 
 ]
