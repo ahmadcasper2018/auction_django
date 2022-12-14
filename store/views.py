4853371687c9da26b795c99cae58e7e1c314c4d7
Ahmad_Salman2021 <ahmad.salman@kuwaitnet.dev>
2022-12-14 12:59:33 +0200

    Fix

--- store/views.py
+++ store/views.py
@@ -14,7 +14,7 @@ from django_filters import rest_framework as filters
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    permission_classes = (ProductPermession,)
+    # permission_classes = (ProductPermession,)
     filter_backends = (filters.DjangoFilterBackend,)
     filterset_class = ProductFilter
 
