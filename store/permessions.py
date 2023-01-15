c030f695b15ccf598b876c7eee7c01b738e40442
Ahmad_Salman2021 <ahmad.salman@kuwaitnet.dev>
2023-01-15 12:13:57 +0200

    feat

--- store/permessions.py
+++ store/permessions.py
@@ -25,3 +25,12 @@ class BrandPermession(BasePermission):
 
     def has_permission(self, request, view):
         return request.user.is_superuser or request.user.is_staff
+
+
+class CompanyPermession(BasePermission):
+
+    def has_permission(self, request, view):
+        return request.user.is_superuser or request.user.is_staff
+
+    def has_object_permission(self, request, view, obj):
+        return request.user.is_superuser or request.user.is_staff
